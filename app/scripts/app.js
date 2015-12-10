837c61b637a5c684eba3d95b416c5e97c6a3a61c
hollisd09 <hollisd09@gmail.com>
2015-12-10 10:56:16 -0600

    Set up initial files with Yeoman. Deleted files that weren't needed. Moved files into lib

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name greetingsApp
+ * @description
+ * # greetingsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('greetingsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
