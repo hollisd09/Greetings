e3a03d024f3c539b5faf70c418a704261bbbc503
hollisd09 <hollisd09@gmail.com>
2015-12-11 15:05:12 -0600

    THINGS ARE WORKING AND IT'S BEAUTIFUL. Clicking on an image in the right side bar populates that image to the card, and makes it draggable. Which may or may not be a word.

--- app/scripts/controllers/draggable.js
+++ app/scripts/controllers/draggable.js
@@ -1,25 +1,19 @@
 angular
   .module('greetingsApp')
   .controller('draggable', function() {
-		
-		$(init);
-		function init() {
-			console.log("RUN!")
-		  $("#makeMeDraggable").draggable ({ 
+
+		$('.btn').click(function(){
+		  var newItem = $(this).clone().appendTo("#content");
+		  $(newItem).addClass("makeMeDraggable");
+		  $(newItem).addClass("ui-draggable");
+		  $(newItem).addClass("ui-draggable-handle");
+
+			$(".makeMeDraggable").draggable ({ 
 		  	containment: "#content", 
 		  	scroll: false 
 		  });
-		};
-
-		$('.btn').click(function(){
-		  $('#clickToAppend').clone().appendTo("#content");
-		  $(this).attr("id","#makeMeDraggable");
 		});
-
-
-		$('.btn-rem').click(function(){
-		$('#iframe1').contents().find('html').html("<h1>").remove();
-		});
+		// $('.btn-rem').click(function(){
+		// $('#iframe1').contents().find('html').html("<h1>").remove();
+		// });
   });
-
-
